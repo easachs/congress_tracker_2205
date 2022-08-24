1235652eeab86a5f97dfebaf6b8e51bf31954559
Eli Sachs <easachs13@gmail.com>
2022-08-24 17:55:19 -0600

    Testing Lesson

--- spec/features/search_congress_members_spec.rb
+++ spec/features/search_congress_members_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Govt Search' do
-  describe 'happy path' do
+  describe 'happy path', vcr: 'search_senate' do
+    # before :each do
+
+    #   json_body = File.read('spec/fixtures/members_of_the_senate.json')
+
+    #   stub_request(:get, "https://api.propublica.org/congress/v1/116/senate/members.json").
+    #      with(
+    #        headers: {
+    #       'Accept'=>'*/*',
+    #       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #       'User-Agent'=>'Faraday v1.8.0',
+    #       'X-Api-Key'=> ENV['govt_api_key']
+    #        }).
+    #      to_return(status: 200, body: json_body, headers: {})
+    # end
+
     it 'allows user to search for govt members' do
       visit root_path
 
